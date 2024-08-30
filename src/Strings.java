50aba2a0ac62808c19af87eb00e82d487ad347db
amitsaini1912 <amitkumarsainig9@gmail.com>
2024-08-30 08:42:45 +0530

    Strings file modified

--- src/Strings.java
+++ src/Strings.java
@@ -3,16 +3,16 @@ public class Strings {
     //Check wheather a string palindrome or not
     public static boolean isPalindrome(String name){
         for(int i=0; i<name.length()/2; i++){
-            if(name.charAt(i) == name.charAt(name.length()-i)){
-                return true;
+            if(name.charAt(i) != name.charAt(name.length()-1-i)){
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] args){
         String name = "racecar";
-        isPalindrome(name);
+        System.out.println(isPalindrome(name));
     }
 
 }
