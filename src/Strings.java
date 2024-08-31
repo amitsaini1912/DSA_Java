1127fd5c0ad6dc4c7dc687e8a48cb3316202eebe
amitsaini1912 <amitkumarsainig9@gmail.com>
2024-08-31 18:51:35 +0530

    finding Shortest path in strings added

--- src/Strings.java
+++ src/Strings.java
@@ -10,9 +10,27 @@ public class Strings {
         return true;
     }
 
+    //Shortest Path Find?
+    public static float pathFinder(String str){
+        int x=0, y=0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == 'N')
+                y +=1;
+            if(str.charAt(i) == 'E')
+                x +=1;
+            if(str.charAt(i) == 'S')
+                y -=1;
+            if(str.charAt(i) == 'W')
+                x -=1;
+        }
+        int x2 = x*x, y2 = y*y;
+        return (float)Math.sqrt(x2+y2);
+
+    }
+
     public static void main(String[] args){
-        String name = "racecar";
-        System.out.println(isPalindrome(name));
+        String name = "EENNNWWS";
+        System.out.println(pathFinder(name));
     }
 
 }
