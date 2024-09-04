3a6f23de0d60ef92c231859d59bd15f6fe0c59ee
amitsaini1912 <amitkumarsainig9@gmail.com>
2024-09-04 12:16:45 +0530

    Bits class modified

--- src/Bits.java
+++ src/Bits.java
@@ -10,8 +10,21 @@ public class Bits {
         }
     }
 
+    //Get ith bit
+    public static int getBit(int num, int i){
+        int bitMask = 1<<(i-1);
+        if( (num & bitMask) == 0 ){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+
     public static void main(String[] args){
-        System.out.println(isEven(7));
+        System.out.println(getBit(4,3));
+        //System.out.println(isEven(7));
+
         //System.out.println(5&6);//3=011, 4=100, 5=101, 6=110  7=111,
 //        System.out.println(5|6);
 //        System.out.println(5^6);
