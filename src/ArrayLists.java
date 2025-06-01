99de31c2247ae1518b63e2e41c26726b02285a7d
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-01 18:34:21 +0530

    ArrayLists file modified

---
+++ src/ArrayLists.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+//        list.set(0,1);
+//        list.set(1,2);
+        System.out.println(list);
+        System.out.println(list.size());
+    }
+}
