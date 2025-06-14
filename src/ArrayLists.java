3e39960c8cee09debf2639a04b9f06deaa6e84c9
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-14 11:28:47 +0530

    Swap Fuction Added

--- src/ArrayLists.java
+++ src/ArrayLists.java
@@ -1,14 +1,24 @@
 import java.util.ArrayList;
 
 public class ArrayLists {
+
+    //SWAP OF TWO NUMBERS IN A ARRAYLIST
+    public static void swap(ArrayList list, int idx1, int idx2){
+        int temp = (int) list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
+
     public static void main(String args[]){
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
         list.add(2);
-        list.add(4);
-//        list.set(0,1);
-//        list.set(1,2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
         System.out.println(list);
-        System.out.println(list.size());
+
+        swap(list, 1,3);
+        System.out.println(list);
     }
 }
