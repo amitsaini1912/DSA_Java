a7843a9c2e379c024afc2bbda5b496cf2bf27db0
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-14 16:21:03 +0530

    PAIR SUM PROBLEM 2 (SORTED & ROTETED ARRAYLIST) SOLVED WITH 2 POINTER APPROACH

--- src/ArrayLists.java
+++ src/ArrayLists.java
@@ -10,8 +10,8 @@ public class ArrayLists {
     }
 
 
-    //PAIR SUM PROBLEM
-    public static boolean isPairSumAvailable(ArrayList<Integer> list,int target){
+    //PAIR SUM PROBLEM FOR SORTED ARRAYLIST
+    public static boolean isPairSum1(ArrayList<Integer> list,int target){
         //BRUTE FORCE APPROACH
         /*int n = list.size();
         for (int i = 0; i < n; i++) {
@@ -39,20 +39,46 @@ public class ArrayLists {
         return false;
     }
 
+
+    //PAIR SUM PROBLEM FOR SORTED & ROTATED ARRAYLIST
+    public static boolean isPairSum2(ArrayList<Integer> list, int target){
+        int n = list.size();
+
+        int bp = -1; //bracking point
+        for (int i = 0; i < n; i++) {
+            if (list.get(i)>list.get(i+1)) {
+                bp = i;
+                break;
+            }
+        }
+
+        int lp = bp+1;
+        int rp = bp;
+        while(lp!=rp){
+            if (list.get(lp)+list.get(rp)==target)
+                return true;
+            if (list.get(lp)+list.get(rp)<target)
+                lp = (lp+1)%n;
+            else
+                rp = (n+rp-1)%n;
+        }
+        return false;
+    }
+
     public static void main(String args[]){
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        list.add(11);
+        list.add(15);
         list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
         System.out.println(list);
 
 //        swap(list, 1,3);
 //        System.out.println(list);
 
-        System.out.println(isPairSumAvailable(list, 11));
+        System.out.println(isPairSum2(list, 100));
 
     }
 }
