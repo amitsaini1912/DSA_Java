349407d272c778c7070774a85cddc2b5bd95da9d
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-15 12:29:46 +0530

    ADD OPERATION ON LINKED LIST

--- src/LinkedLists_Implementation.java
+++ src/LinkedLists_Implementation.java
@@ -15,10 +15,35 @@ public class LinkedLists_Implementation {
     public static Node tail;
 
 
+    //ADD AT FIRST
+    public void addFirst(int data){
+        //step1 - create new node
+        Node newNode = new Node(data);
+        if (head==null){
+            head = tail = newNode;
+            return;
+        }
+        //step2 - new node next -> head
+        newNode.next = head;
+        //step3 - head -> new node
+        head = newNode;
+    }
+    //ADD AT LAST
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if (head==null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next= newNode;
+        tail = newNode;
+    }
+
+
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
-        ll.add(2);
-        ll.add(4);
+        ll.addLast(1);
+        ll.addLast(2);
         System.out.println(ll);
     }
 }
