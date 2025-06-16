9c985c363821234b8b482cfb2d8113218f29fb26
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-16 17:34:47 +0530

    MERGE SORT ON LINKED LIST

--- src/LinkedLists_Implementation.java
+++ src/LinkedLists_Implementation.java
@@ -268,21 +268,66 @@ public class LinkedLists_Implementation {
         }
     }
 
+
+    //MERGE SORT ON LINKED LIST
+    private Node merge(Node head1, Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+        while(head1!=null && head2!=null){
+            if(head1.data<=head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        //below loops runs if left part and right parts are not equal
+        while(head1!=null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2!=null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+    public Node mergeSort(Node head){
+        if(head==null || head.next==null)
+            return head;
+        //step1 - find mid
+        Node slow = head;
+        Node fast = head.next; //here head.next used bcz -> we have two mids in even so we want 1st
+        while(fast!=null && fast.next!=null){
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+        }
+        Node mid = slow;
+        //step2 - left & right Merge sort
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+        //step3 - merge
+        return merge(newLeft, newRight);
+    }
+
+
     public static void main(String args[]){
-        LinkedList ll = new LinkedList();
-        head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = head;
-        System.out.println(iscycle());
-        remCycle();
-        System.out.println(iscycle());
-        //     LinkedLists_Implementation ll = new LinkedLists_Implementation();
-//        ll.addLast(2);
-//        ll.addLast(4);
-//        ll.addLast(5);
-//        ll.addLast(6);
-//        ll.add(2,3);
-//        ll.print();
+        //LinkedList ll = new LinkedList();
+        LinkedLists_Implementation ll = new LinkedLists_Implementation();
+        ll.addLast(5);
+        ll.addLast(4);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.add(2,3);
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
     }
 }
