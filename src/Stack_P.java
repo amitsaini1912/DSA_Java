a7304700df5e38097a9b329671d5a45f38ffcdf6
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-17 14:17:03 +0530

    REVERSE A STACK USING RECURSION WITHOUT USING EXTRA SPACE

--- src/Stack_P.java
+++ src/Stack_P.java
@@ -31,18 +31,24 @@ public class Stack_P {
     }
 
 
+    //REVERSE A STACK USING RECURSION WITHOUT USING EXTRA SPACE
+    public static void reverseStack(Stack<Integer>s){
+        if(s.isEmpty()) return;
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBootom(s,top);
+    }
+
     public static void main(String args[]){
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
 
-        /*pushAtBootom(s,4);
+        reverseStack(s);
         while(!s.isEmpty()){
             System.out.println(s.pop());
-        }*/
+        }
 
-        String str = "abcd";
-        System.out.println(reverseStr(str));
     }
 }
