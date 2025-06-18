ef9fb033b8b8a630dca5f584d768afbd85998826
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-18 14:51:12 +0530

    CIRCULAR QUEUE IMPLEMENTATION USING ARRAYS

--- src/Queue_Implementation_Arrays.java
+++ src/Queue_Implementation_Arrays.java
@@ -39,15 +39,63 @@ public class Queue_Implementation_Arrays {
             return arr[0];
         }
     }
-    public static void main(String args[]){
-        Queue q = new Queue(4);
-        q.add(1);
-        q.add(2);
-        q.add(3);
 
-        while (!q.isEmpty()){
-            System.out.println(q.peek());
-            q.remove();
+
+    //CIRCULAR QUEUE IMPLEMENTATION USING ARRAYS
+    static class CQueue{
+        static int arr[];
+        static int size;
+        static int front;
+        static int rear;
+        CQueue(int n){
+            arr = new int[n];
+            size = n;
+            front = -1;
+            rear = -1;
+        }
+        public boolean isEmpty(){
+            return  (rear==-1 && front==-1);
+        }
+        public boolean isFull(){
+            return (rear+1)%size==front;
+        }
+        public void add(int data){
+            if (isFull()){
+                System.out.println("Queue is Full");
+                return;
+            }
+            if (front==-1)
+                front=0;  //frist time add
+            rear = (rear+1)%size;
+            arr[rear]=data;
+        }
+        public int remove(){
+            if (isEmpty())
+                return -1;
+            int result = arr[front];
+            if (rear==front)
+                rear=front=-1; //last element remove
+            else
+                front = (front+1)%size;
+            return result;
+        }
+        public int peek(){
+            if (isEmpty())
+                return -1;
+            return arr[front];
+        }
+    }
+
+
+    public static void main(String args[]){
+        CQueue CQ = new CQueue(4);
+        CQ.add(1);
+        CQ.add(2);
+        CQ.add(3);
+
+        while (!CQ.isEmpty()){
+            System.out.println(CQ.peek());
+            CQ.remove();
         }
     }
 }
