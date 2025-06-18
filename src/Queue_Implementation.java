1ade4e9739932d16202a181f92616835880e135c
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-18 18:36:01 +0530

    STACK IMPLEMENTATION USING TWO QUEUES

--- src/Queue_Implementation.java
+++ src/Queue_Implementation.java
@@ -1,13 +1,15 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Queue_Implementation {
 
     //QUEUE IMPLEMENTATION USING ARRAYS
-    static class Queue{
+    static class QueueA{
         static int arr[];
         static int size;
         static int rear;
-        Queue(int n){
+        QueueA(int n){
             arr = new int[n];
             size = n;
             rear = -1;
@@ -165,15 +167,73 @@ public class Queue_Implementation {
     }
 
 
-    public static void main(String args[]){
-        Queue2S q = new Queue2S();
-        q.add(1);
-        q.add(2);
-        q.add(3);
+    //STACK IMPLEMENTATION USING TWO QUEUES
+    static class StackC2{
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
 
-        while (!q.isEmpty()){
-            System.out.println(q.peek());
-            q.remove();
+        public boolean isEmpty(){
+            return q1.isEmpty() && q2.isEmpty();
+        }
+        //Add data in non empty queue
+        public void add(int data){
+            if(!q1.isEmpty())
+                q1.add(data);
+            else
+                q2.add(data);
+        }
+        //Remove
+        public int pop(){
+            if (isEmpty())
+                return -1;
+            int top = -1;
+            if(!q1.isEmpty()){  //case 1 - when transfer q1 to q2
+                while (!q1.isEmpty()){
+                    top = q1.remove();
+                    if(q1.isEmpty())
+                        break;
+                    q2.add(top);
+                }
+            }else{ //case 2 - when transfer q2 to q1
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty())
+                        break;
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+        //peek
+        public int peek(){
+            if (isEmpty())
+                return -1;
+            int top = -1;
+            if(!q1.isEmpty()){  //case 1 - when transfer q1 to q2
+                while (!q1.isEmpty()){
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            }else{ //case 2 - when transfer q2 to q1
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+    }
+
+
+    public static void main(String args[]){
+        StackC2 s = new StackC2();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+
+        while (!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
         }
     }
 }
