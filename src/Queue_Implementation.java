c0ae2d2c075f46802f8d3097aa2380015c6b4daa
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-18 17:09:06 +0530

    QUEUE IMPLEMENTATION USING TWO STACKS

--- src/Queue_Implementation.java
+++ src/Queue_Implementation.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Queue_Implementation {
 
     //QUEUE IMPLEMENTATION USING ARRAYS
@@ -133,8 +135,38 @@ public class Queue_Implementation {
     }
 
 
+    //QUEUE IMPLEMENTATION USING TWO STACKS
+    static class Queue2S{
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        public boolean isEmpty(){
+            return s1.isEmpty();
+        }
+        public void add(int data){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+            while (!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+        }
+        public int remove(){
+            if (s1.isEmpty())
+                return -1;
+            return s1.pop();
+        }
+        public int peek(){
+            if (s1.isEmpty())
+                return -1;
+            return s1.peek();
+        }
+    }
+
+
     public static void main(String args[]){
-        QueueLL q = new QueueLL();
+        Queue2S q = new Queue2S();
         q.add(1);
         q.add(2);
         q.add(3);
