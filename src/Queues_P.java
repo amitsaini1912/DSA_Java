96c0a8622d50b1ab0a95cfe2e8d620b38475e3e1
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-19 12:49:56 +0530

    INTERLEAVE 2 HALVES OF A QUEUE (EVEN LENGTH)

--- src/Queues_P.java
+++ src/Queues_P.java
@@ -20,8 +20,35 @@ public class Queues_P {
         }
     }
 
+
+    //INTERLEAVE 2 HALVES OF A QUEUE (EVEN LENGTH)
+    public static void interleave(Queue<Integer> q){
+        Queue<Integer> firstHalf = new LinkedList<>();
+        int size = q.size();
+        for (int i = 0; i < size/2; i++) {
+            firstHalf.add(q.remove());
+        }
+        while(!firstHalf.isEmpty()){
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
+    }
+
+
     public static void main(String args[]){
-        String str = "aabccxb";
-        firstNRL(str);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        interleave(q);
+        while (!q.isEmpty()){
+            System.out.print(q.peek()+" ");
+            q.remove();
+        }
     }
 }
