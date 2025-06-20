2116cdbd4c76e79bb071c0a51cfe95755916a1d6
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-20 08:57:43 +0530

    GREEDY ALGORITHMS: FRACTIONAL KNAPSACK PROBLEM

--- src/Greedy_Algorithms.java
+++ src/Greedy_Algorithms.java
@@ -33,7 +33,39 @@ public class Greedy_Algorithms {
     }
 
 
+    //GREEDY ALGORITHMS: FRACTIONAL KNAPSACK PROBLEM
+    public static void fracKnapsack(){
+        int val[] = {60,100,120};
+        int weight[] = {10,20,30};
+        int w = 50;
+
+        double ratio[][] = new double[val.length][2];
+        for (int i = 0; i < val.length; i++) {
+            ratio[i][0] = i;
+            ratio[i][1] = val[i]/(double)weight[i];
+        }
+
+        Arrays.sort(ratio, comparator.comparingDouble(o -> o[1]));
+
+        int capacity = w;
+        int finalVal = 0;
+        for (int i = ratio.length-1; i>=0; i--){
+            int idx = (int)ratio[i][0];
+            if(capacity>=weight[idx]){
+                finalVal += val[idx];
+                capacity -= weight[idx];
+            }else{
+                finalVal += (ratio[i][1]*capacity);
+                capacity = 0;
+                break;
+            }
+        }
+        System.out.println("Final maximum Value in knapsack: "+finalVal);
+    }
+
+
+
     public static void main(String args[]){
-        maxActSelection();
+        fracKnapsack();
     }
 }
