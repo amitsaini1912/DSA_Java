65d66d0d5f4bdd5b9b3a3b939df25a55fe2af0fc
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-21 20:32:34 +0530

    BINARY TREES: LOWEST COMMON ANCESTOR (Approach-1) => TC - O(n) SC - O(n)

--- src/BinaryTrees.java
+++ src/BinaryTrees.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class BinaryTrees {
     static class Node {
@@ -218,6 +215,41 @@ public class BinaryTrees {
     }
 
 
+    //BINARY TREES: LOWEST COMMON ANCESTOR (Approach-1) => TC - O(n) SC - O(n)
+    public static boolean getPath(Node root, int n, ArrayList<Node>path){
+        if (root==null)
+            return false;
+
+        path.add(root);
+
+        if (root.data==n)
+           return true;
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft||foundRight)
+            return true;
+
+        path.remove(path.size()-1); //current root is not part of path
+        return false;
+    }
+    public static int lca(Node root, int n1, int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root,n1,path1);
+        getPath(root,n2,path2);
+
+        int i = 0;
+        for (; i<path1.size() && i<path2.size(); i++) {
+            if (path1.get(i)!=path2.get(i))
+                break;
+        }
+        return path1.get(i-1).data;
+    }
+
+
     public static void main(String args[]){
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
@@ -240,6 +272,6 @@ public class BinaryTrees {
                   / \   \
                  4   5   7
          */
-        kLevel(root1, 1, 3);
+        System.out.println(lca(root1,4,7));
     }
 }
