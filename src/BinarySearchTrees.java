73408a2e8628b6c7806c649263429d1fe648cf45
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-22 17:43:24 +0530

    BINARY SEARCH TREES: MIRROR A BST

--- src/BinarySearchTrees.java
+++ src/BinarySearchTrees.java
@@ -130,12 +130,29 @@ public class BinarySearchTrees {
     }
 
 
+    //BINARY SEARCH TREES: MIRROR A BST
+    public static Node mirrorBST(Node root){
+        if(root==null)
+            return null;
+
+        Node LM = mirrorBST(root.left);
+        Node RM = mirrorBST(root.right);
+
+        root.left = RM;
+        root.right = LM;
+        return root;
+    }
+
+
     public static void main(String args[]){
         int values[] = {5,1,3,4,2,7};
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root,values[i]);
         }
-        System.out.println(isValidBST(root,null,null));
+        inorderTraversal(root);
+        System.out.println(" ");
+        mirrorBST(root);
+        inorderTraversal(root);
     }
 }
