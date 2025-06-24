f035300dca6ef5a55d841e1b8570427e8ee6b9b1
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-24 15:48:47 +0530

    HEAP: HEAP SORT - ASCENDING ORDER SORT (BY CREATING MAX HEAP)

--- src/Heap_Implementation.java
+++ src/Heap_Implementation.java
@@ -71,44 +71,44 @@ public class Heap_Implementation {
     }
 
 
-        public static void heapify(int arr[], int i, int size) {
-            int left = 2 * i + 1;
-            int right = 2 * i + 2;
-            int maxInt = i;
+    //HEAP: HEAP SORT - ASCENDING ORDER SORT (BY CREATING MAX HEAP)
+    public static void heapify(int arr[], int i, int size) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int maxInt = i;
 
-            if (left < size && arr[left] > arr[maxInt]) {
-                maxInt = left;
-            }
-
-            if (right < size && arr[right] > arr[maxInt]) {
-                maxInt = right;
-            }
-
-            if (maxInt != i) {
-                int temp = arr[i];
-                arr[i] = arr[maxInt];
-                arr[maxInt] = temp;
-
-                heapify(arr, maxInt, size);
-            }
+        if (left < size && arr[left] > arr[maxInt]) {
+            maxInt = left;
         }
 
-        public static void heapSort(int arr[]) {
-            //step1 - build maxHeap
-            int n = arr.length;
-            for (int i = n / 2; i >= 0; i--) {
-                heapify(arr, i, n);
-            }
-
-            //step2 - push largest at end
-            for (int i = n - 1; i > 0; i--) {
-                int temp = arr[0];
-                arr[0] = arr[i];
-                arr[i] = temp;
-
-                heapify(arr, 0, i);
-            }
+        if (right < size && arr[right] > arr[maxInt]) {
+            maxInt = right;
         }
+
+        if (maxInt != i) {
+            int temp = arr[i];
+            arr[i] = arr[maxInt];
+            arr[maxInt] = temp;
+
+            heapify(arr, maxInt, size);
+        }
+    }
+    public static void heapSort(int arr[]) {
+        //step1 - build maxHeap
+        int n = arr.length;
+        for (int i = n / 2; i >= 0; i--) {
+            heapify(arr, i, n);
+        }
+
+        //step2 - push largest at end
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, 0, i);
+        }
+    }
 
 
 
@@ -121,9 +121,10 @@ public class Heap_Implementation {
         h.add(10);
         h.add(6);
 
-        while (!h.isEmpty()){
-            System.out.println(h.peek());
-            h.remove();
+        int arr[] = {1,2,4,5,3};
+        heapSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+ " ");
         }
     }
 
