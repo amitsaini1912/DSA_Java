e103983f1f7b9a5842f751681ca51c2132a08c6d
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-06-26 14:02:03 +0530

    TRIES: SEARCH IN TRIES

--- src/Tries.java
+++ src/Tries.java
@@ -9,9 +9,12 @@ public class Tries {
             }
         }
     }
+
+    public static Node root = new Node();
+
     //INSERT IN A TRIES
     public static void insert(String word){
-        Node curr = new Node();
+        Node curr = root;
         for (int level = 0; level < word.length(); level++) {
             int idx = word.charAt(level)-'a';
             if (curr.children[idx]==null){
@@ -23,6 +26,20 @@ public class Tries {
     }
 
 
+    //SEARCH IN TRIES
+    public static boolean search(String word){
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level)-'a';
+            if (curr.children[idx]==null){
+                return false;
+            }
+            curr =  curr.children[idx];
+        }
+        return curr.eow;
+    }
+
+
     public static void main(String args[]){
         String word[] = {"the","a", "there", "their", "any", "thee"};
 
@@ -30,6 +47,6 @@ public class Tries {
             insert(word[i]);
         }
 
-
+        System.out.println(search("thee"));
     }
 }
