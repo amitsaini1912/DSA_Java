7da65de1772c9430c68c9f589bd0600ca07019d7
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-07-01 15:05:28 +0530

    DYNAMIC PROGRAMING: CLIMBING STAIRS - ITERARTION TABULATION

--- src/DP_Basics.java
+++ src/DP_Basics.java
@@ -47,11 +47,29 @@ public class DP_Basics {
     }
 
 
+    //DYNAMIC PROGRAMING: CLIMBING STAIRS - ITERARTION TABULATION
+    public static int climbStairT(int n){
+        //step1 Initialization
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+
+        //step2 - Calculations (Filling)
+        for (int i = 1; i <= n; i++) {
+            if (i==1)
+               dp[i] = dp[i-1] + 0;
+            else
+                dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+
     public static void main(String args[]){
 
         int n = 5;
         int dp[] = new int[n+1];
-        System.out.println(climbStair(n, dp));
+        System.out.println(climbStairT(n));
     }
 
 }
