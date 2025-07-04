fa8b09f8e92d9d0a144d0c045cf7318d1ad1c73e
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-07-04 20:13:20 +0530

    DYNAMIC PROGRAMING: LCS (LONGEST COMMON SUBSEQUENCE) - RECURSION -> O(2^N)

--- src/DP_Basics.java
+++ src/DP_Basics.java
@@ -183,12 +183,26 @@ public class DP_Basics {
         return dp[n][W];
     }
 
-    public static void main(String args[]){
-        int val[] = {15,14,10,45,30};
-        int wt[] = {2,3,1,3,4};
-        int W = 7;
 
-        System.out.println(unBoundedKnapsackT(val, wt, W));
+    //DYNAMIC PROGRAMING: LCS (LONGEST COMMON SUBSEQUENCE) - RECURSION -> O(2^N)
+    public static int lcsR(String str1, String str2, int n, int m){
+        if(n==0 || m==0)
+            return 0;
+
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+            return lcsR(str1, str2, n-1, m-1) + 1;
+        }else {
+            int ans1 = lcsR(str1, str2, n-1, m);
+            int ans2 = lcsR(str1, str2, n, m-1);
+            return Math.max(ans1, ans2);
+        }
+    }
+
+    public static void main(String args[]){
+        String str1 = "abcde";
+        String str2 = "ace";
+
+        System.out.println(lcsR(str1, str2, 5, 3));
     }
 
 }
