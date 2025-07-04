b331267a39e3a8c77e78dbae018f2f14a356f1af
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-07-04 20:19:11 +0530

    DYNAMIC PROGRAMING: LCS (LONGEST COMMON SUBSEQUENCE) - MEMOIZATION -> O(n*m)

--- src/DP_Basics.java
+++ src/DP_Basics.java
@@ -198,11 +198,38 @@ public class DP_Basics {
         }
     }
 
+
+    //DYNAMIC PROGRAMING: LCS (LONGEST COMMON SUBSEQUENCE) - MEMOIZATION -> O(n*m)
+    public static int lcsM(String str1, String str2, int n, int m, int dp[][]){
+        if(n==0 || m==0)
+            return 0;
+
+        if (dp[n][m]!=-1)
+            return dp[n][m];
+
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+            return dp[n][m] = lcsM(str1, str2, n-1, m-1, dp) + 1;
+        }else {
+            int ans1 = lcsM(str1, str2, n-1, m, dp);
+            int ans2 = lcsM(str1, str2, n, m-1, dp);
+            return dp[n][m] = Math.max(ans1, ans2);
+        }
+    }
+
     public static void main(String args[]){
         String str1 = "abcde";
         String str2 = "ace";
+        int n = str1.length();
+        int m = str2.length();
 
-        System.out.println(lcsR(str1, str2, 5, 3));
+        int dp[][] = new int[n+1][m+1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(lcsM(str1, str2, n, m, dp));
     }
 
 }
