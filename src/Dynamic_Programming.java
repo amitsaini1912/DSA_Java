803be3f11774c558bf8f2ee6ba417d535f3f3cc8
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-07-05 21:39:00 +0530

    DYNAMIC PROGRAMING: WILDCARD MATCHING PROBLEM - TABULATION -> O(n*m)

--- src/Dynamic_Programming.java
+++ src/Dynamic_Programming.java
@@ -1,4 +1,4 @@
-public class DP_Basics {
+public class Dynamic_Programming {
 
     //Dynamic Programing: Fibonacci Series Using Recursion Memoization
     public static int fib(int n, int[] dp){
@@ -248,9 +248,42 @@ public class DP_Basics {
     }
 
 
+    //DYNAMIC PROGRAMING: WILDCARD MATCHING PROBLEM - TABULATION -> O(n*m)
+    public static boolean isMatch(String s, String p){
+        int n = s.length();
+        int m = p.length();
+        boolean dp[][] = new boolean[n+1][m+1];
+
+        //Initialization
+        dp[0][0] = true;
+        for (int i = 1; i < n+1; i++) {
+            dp[i][0] = false;
+        }
+        for (int j = 1; j < m+1; j++) {
+            if (p.charAt(j-1)=='*')
+                dp[0][j] = dp[0][j-1];
+        }
+
+        //bottom up
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < m+1; j++) {
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p.charAt(j-1)=='*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+
     public static void main(String args[]){
-        String str1 = "abcde";
-        String str2 = "ace";
+        String str1 = "abc";
+        String str2 = "**d";
         int n = str1.length();
         int m = str2.length();
 
@@ -261,7 +294,7 @@ public class DP_Basics {
             }
         }
 
-        System.out.println(lcsT(str1, str2, n, m));
+        System.out.println(isMatch(str1, str2));
     }
 
 }
